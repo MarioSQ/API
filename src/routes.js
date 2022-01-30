a043bae040acf63920a307a6795d180ae6ea9b5a
Mario Quinello <marioquinello@hotmail.com>
2022-01-30 13:17:39 -0300

    Atualizar comentário rota Buscar Mensagens

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const env_whatsapp = require('../src/env_whatsapp')
 const env_email = require('../src/env_email')
 var save = []
 
-// Buscar mensagens enviadas ao cliente - UC04: Consultar Status Mensagem
+// Rota para buscar mensagens enviadas ao cliente - UC04: Consultar Status Mensagem
 routes.get('/consultar', function(req, res){
     fs.readFile('envios.json', 'utf8', function(err, data){ 
       if (err) {
